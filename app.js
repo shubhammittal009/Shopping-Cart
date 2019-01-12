cea963f345fc03adc223468849f1426703b3e8f2
Shubham Mittal <shubhammittal5114@gmail.com>
2019-01-12 12:47:04 +0530

    first commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
